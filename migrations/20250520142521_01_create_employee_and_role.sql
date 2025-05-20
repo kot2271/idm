47f796a278ba807ea7cb8f91c2bc4068f082af22
mini-kotan <kot2271@yandex.ru>
2025-05-20 17:45:15 +0300

    added migration from goose.

--- migrations/20250520142521_01_create_employee_and_role.sql
+++ migrations/20250520142521_01_create_employee_and_role.sql
@@ -1,22 +1,29 @@
--- Create role table
-CREATE TABLE IF NOT EXISTS "role" (
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS role (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name TEXT NOT NULL,
     description TEXT,
     status BOOLEAN DEFAULT TRUE,
-    parent_id BIGINT REFERENCES "role"(id),
+    parent_id BIGINT REFERENCES role(id),
     created_at TIMESTAMPTZ DEFAULT NOW(),
     updated_at TIMESTAMPTZ DEFAULT NOW()
 );
 
--- Create employee table
 CREATE TABLE IF NOT EXISTS employee (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name TEXT NOT NULL,
     email TEXT UNIQUE NOT NULL,
     position TEXT,
     department TEXT,
-    role_id BIGINT REFERENCES "role"(id),
+    role_id BIGINT REFERENCES role(id),
     created_at TIMESTAMPTZ DEFAULT NOW(),
     updated_at TIMESTAMPTZ DEFAULT NOW()
 );
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS role;
+-- +goose StatementEnd
